2cbbb73d31907a1c4de271c222f5478e85842a79
Yampier Medina <jpmti2016@gmail.com>
2021-06-21 00:12:33 -0400

    Test posts page SEO

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,10 +4,15 @@ export default {
     locale: "en_US",
     url: "https://portfolio-nextjs-mdx-a5x23t4uf-jpmti2016.vercel.app/",
     site_name: "Yampier Medina personal website (software engineer)",
+    title: "Yampier Medina personal website (software engineer)",
+    description:
+      "I am a Software engineer that know javascript, reactjs, nextjs, graphql, sql, aws amplify, css, html5, taildwindcss, bulma, postcss and git. Searching for job, 100% remote. Hire me.",
+    url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app",
+    image:
+      "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/personal-portfolio_559.jpg",
   },
   twitter: {
     handle: "@jpmti2016",
-    site: "@jpmti2016",
     cardType: "summary_large_image",
   },
 };
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,15 @@
+import { NextSeo } from "next-seo";
+
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div>
+      <NextSeo
+        title="Amazing blog title"
+        description="Amazing blog description"
+        url="https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/posts"
+        image="https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/github-jobs.jpg"
+      />
+      Blog
+    </div>
+  );
 }
