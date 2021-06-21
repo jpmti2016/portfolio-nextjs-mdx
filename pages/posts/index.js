3f29745018b9be090d4b677620d9597de09ad938
Yampier Medina <jpmti2016@gmail.com>
2021-06-21 01:00:27 -0400

    Test SEO images

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -8,6 +8,28 @@ export default function Blog() {
         description="Amazing blog description"
         url="https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/posts"
         image="https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/github-jobs.jpg"
+        openGraph={{
+          url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/posts",
+          title: "Open Graph Title",
+          description: "Open Graph Description",
+          images: [
+            {
+              url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/personal-portfolio.jpg",
+              alt: "Og Image Alt Second",
+            },
+            {
+              url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/natureatyourfeet.jpg",
+              alt: "Og Image Alt",
+            },
+            {
+              url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/trackingmyfinance.jpg",
+            },
+            {
+              url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/image-galery.jpg",
+            },
+          ],
+          site_name: "SiteName",
+        }}
       />
       Blog
     </div>
