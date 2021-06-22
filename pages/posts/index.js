912227367da5db341b79f0dc8cc553eeb36763e8
Yampier Medina <jpmti2016@gmail.com>
2021-06-22 03:21:42 -0400

    Change domain

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -6,26 +6,26 @@ export default function Blog() {
       <NextSeo
         title="Amazing blog title"
         description="Amazing blog description"
-        url="https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/posts"
-        image="https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/github-jobs.jpg"
+        url="https://portfolio-nextjs-mdx-be3ndvkje-jpmti2016.vercel.app/posts"
+        image="https://portfolio-nextjs-mdx-be3ndvkje-jpmti2016.vercel.app/public/images/github-jobs.jpg"
         openGraph={{
-          url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/posts",
+          url: "https://portfolio-nextjs-mdx-be3ndvkje-jpmti2016.vercel.app/posts",
           title: "Open Graph Title",
           description: "Open Graph Description",
           images: [
             {
-              url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/public/images/personal-portfolio.jpg",
+              url: "https://portfolio-nextjs-mdx-be3ndvkje-jpmti2016.vercel.app/public/images/personal-portfolio.jpg",
               alt: "Og Image Alt Second",
             },
             {
-              url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/public/images/natureatyourfeet.jpg",
+              url: "https://portfolio-nextjs-mdx-be3ndvkje-jpmti2016.vercel.app/public/images/natureatyourfeet.jpg",
               alt: "Og Image Alt",
             },
             {
-              url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/public/images/trackingmyfinance.jpg",
+              url: "https://portfolio-nextjs-mdx-be3ndvkje-jpmti2016.vercel.app/public/images/trackingmyfinance.jpg",
             },
             {
-              url: "https://portfolio-nextjs-mdx-kcjeqcm0m-jpmti2016.vercel.app/public/images/image-galery.jpg",
+              url: "https://portfolio-nextjs-mdx-be3ndvkje-jpmti2016.vercel.app/public/images/image-galery.jpg",
             },
           ],
           site_name: "SiteName",
