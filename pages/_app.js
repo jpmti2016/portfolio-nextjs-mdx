4f8e8fe776c6d8145a9bbf7ccc100fe3e034b0c6
Yampier Medina <jpmti2016@gmail.com>
2021-07-12 06:01:32 -0400

    Fix router events clear effect

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
 
     router.events.on("routeChangeComplete", handleRouteChange);
 
-    return router.events.off("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
   }, [router.events]);
   return (
     <ThemeProvider attribute="class">
