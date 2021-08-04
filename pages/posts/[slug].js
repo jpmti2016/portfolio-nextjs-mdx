0b3c683275c985b1f75c4411159960ed070bb56e
Yampier Medina <jpmti2016@gmail.com>
2021-08-04 10:17:56 -0400

    Create the same canonical url without www

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -20,11 +20,8 @@ export default function Post({ source, frontMatter, timeToRead }) {
         <title>{frontMatter?.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={frontMatter?.description} name="description" />
-        <meta
-          property="og:url"
-          content={`https://www.jpmti2016.com${asPath}`}
-        />
-        <link rel="canonical" href={`https://www.jpmti2016.com${asPath}`} />
+        <meta property="og:url" content={`https://jpmti2016.com${asPath}`} />
+        <link rel="canonical" href={`https://jpmti2016.com${asPath}`} />
         <meta property="og:type" content={frontMatter?.type} />
         <meta property="og:site_name" content={frontMatter?.site_name} />
         <meta property="og:description" content={frontMatter?.description} />
