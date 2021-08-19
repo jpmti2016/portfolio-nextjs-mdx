97676767bd7bcc9c7c4c7ae2aef98957085130de
Yampier Medina <jpmti2016@gmail.com>
2021-08-19 01:59:32 -0400

    Copy paste

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -1,51 +1,43 @@
+import { useState } from "react";
 import axios from "axios";
-import React, { useRef, useState } from "react";
 
 export default function Subscribe() {
   const [email, setEmail] = useState("");
-  const [errorMessage, setErrorMessage] = useState(null);
   const [state, setState] = useState("IDLE");
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  const subscribe = async (e) => {
+  const subscribe = async () => {
     setState("LOADING");
     setErrorMessage(null);
-
     try {
-      const response = await axios.post("/api/subscribe", {
-        email,
-      });
+      const response = await axios.post("/api/newsletter", { email });
       setState("SUCCESS");
-      inputEl.current.value = "";
-
-      console.log("FE response", response);
-    } catch (error) {
-      setErrorMessage(error.response.data.error);
+    } catch (e) {
+      setErrorMessage(e.response.data.error);
       setState("ERROR");
     }
   };
 
   return (
-    <div className="flex flex-col items-center w-full p-8 mt-8 border border-blue-400 border-solid rounded-sm">
-      <h2 className="text-2xl font-bold text-center">
-        Keep in the loop for new articles
+    <div className="flex flex-col items-center w-full p-8 mt-8 border border-gray-500 border-solid rounded-sm">
+      <h2 className="text-3xl font-bold text-center">
+        I also have a newsletter!
       </h2>
       <p className="w-4/5 mt-2 font-light leading-relaxed text-center">
-        I will drop in your inbox verery two week
+        It includes intersting stuff about tech and will arrive to your mailbox
+        no more than once every 2 weeks
       </p>
-      <div className="flex flex-col justify-center w-1/2 mt-5 lg:2/3 lg:flex-row">
+      <div className="flex flex-col justify-center w-1/2 mt-5 lg:w-2/3 lg:flex-row">
         <input
-          className="w-full px-4 py-2 mb-2 leading-tight text-gray-700 border border-blue-400 rounded appearance-none lg:mb-0 lg:w-2/3 focus:outline-none focus:border-blue-600"
+          className="w-full px-4 py-2 mb-2 leading-tight text-gray-700 border border-gray-500 rounded appearance-none lg:mb-0 lg:w-2/3 focus:outline-none focus:border-gray-600"
           type="text"
-          placeholder="email@domain.com"
+          placeholder="Enter Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-
         <button
-          className={`lg:ml-2 w-full lg:w-1/3 shadow bg-blue-600 focus:shadow focus:outline-none text-center text-blue-900 font-bold py-2 px-4 rounded flex ${
-            state === "LOADING"
-              ? "bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"
-              : ""
+          className={`lg:ml-2 w-full lg:w-1/3 shadow bg-brand2 focus:shadow-outline focus:outline-none text-center text-white font-bold py-2 px-4 rounded flex ${
+            state === "LOADING" ? "button-gradient-loading" : ""
           }`}
           type="button"
           disabled={state === "LOADING"}
