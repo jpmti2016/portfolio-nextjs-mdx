5367d11a78dcef170769aa1732ab102c825a170a
Yampier Medina <jpmti2016@gmail.com>
2021-12-27 06:46:25 -0500

    Redirect any language change to home

--- components/ChangeLanguage.js
+++ components/ChangeLanguage.js
@@ -13,8 +13,8 @@ export default function ChangeLanguage() {
     if (lng === lang) return null;
 
     return (
-      <Link href={asPath} locale={lng} key={lng} className="">
-        <a className="flex items-center px-1 ml-4 rounded-md sm:text-2xl ring-1 dark:ring-gray-100 hover:text-gray-100 hover:bg-gray-800">
+      <Link href="/" locale={lng} key={lng} className="">
+        <a className="flex items-center px-1 ml-4 rounded-md sm:text-2xl ring-1 ring-gray-800 dark:ring-gray-100 hover:text-gray-100 hover:bg-gray-800">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="w-5 h-5 sm:w-7 sm:h-7"
