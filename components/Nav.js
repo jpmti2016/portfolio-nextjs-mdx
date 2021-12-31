661f7ce5e3810360f1ca4c89f9319c8ed2560648
Yampier Medina <jpmti2016@gmail.com>
2021-12-31 04:59:23 -0500

    Makes the images responsive and with a width and height set

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ import { useTheme } from "next-themes";
 import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import jpmti2016 from "../public/images/jpmti2016.jpeg";
+import jpmti2016 from "../public/images/jpmti2016-100x100.jpg";
 import Menu from "./Menu";
 import MobileButton from "./MobileButton";
 import ChangeLanguage from "./ChangeLanguage";
@@ -36,6 +36,8 @@ export default function Nav() {
                 className="rounded-full"
                 layout="responsive"
                 alt="Yampier Medina avatar"
+                height={100}
+                width={100}
               />
             </div>
           </a>
