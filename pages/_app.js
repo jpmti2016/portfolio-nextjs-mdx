b9b823e1f0907f7cbf7c24a25ad56bb39674a02a
Yampier Medina <jpmti2016@gmail.com>
2022-03-11 18:41:35 -0500

    Added umami analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <Script
+        strategy="afterInteractive"
+        data-website-id="4ba2c90b-d8db-4811-a7ba-a99cdc097786"
+        src="https://umami-three-neon.vercel.app/umami.js"
+      />
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
